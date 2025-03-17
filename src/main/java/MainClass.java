088b8eb86a9996d12eccb698df3a6b27a9f58f41
ahmadsaaad <ahmad.saaad@gmail.com>
2025-03-17 11:46:53 +0000

    create main class

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+
+    public static void main(String[] args){
+        String myText= "Hi my name is Ahmed";
+
+        System.out.println(AgaUtils.getTextUppercase(myText));
+    }
+}
